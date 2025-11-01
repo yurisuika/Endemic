0d832c3e15b52d86059c8cd8696bc7cde82f581a
yurisuika <yurisuika@proton.me>
2025-11-01 02:46:42 -0700

    publish branch

--- src/main/java/dev/yurisuika/endemic/config/Config.java
+++ src/main/java/dev/yurisuika/endemic/config/Config.java
@@ -2,7 +2,7 @@ package dev.yurisuika.endemic.config;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import net.fabricmc.loader.api.FabricLoader;
+import net.minecraftforge.fml.loading.FMLPaths;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -11,7 +11,7 @@ import java.nio.file.Files;
 
 public class Config {
 
-    public static File file = new File(FabricLoader.getInstance().getConfigDir().toFile(), "endemic.json");
+    public static File file = new File(FMLPaths.CONFIGDIR.get().toFile(), "endemic.json");
     public static Gson gson = new GsonBuilder().enableComplexMapKeySerialization().setPrettyPrinting().disableHtmlEscaping().create();
     public static Options options = new Options();
 
