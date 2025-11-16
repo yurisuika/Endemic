6fd86bee814218c43ac75c2caf8a0b3b9356ea70
yurisuika <yurisuika@proton.me>
2025-11-16 08:37:52 -0800

    Move sapling configs into separate files + Create sapling registry

--- src/main/java/dev/yurisuika/endemic/Endemic.java
+++ src/main/java/dev/yurisuika/endemic/Endemic.java
@@ -2,10 +2,12 @@ package dev.yurisuika.endemic;
 
 import dev.yurisuika.endemic.config.Config;
 import dev.yurisuika.endemic.data.EndemicDatapackProvider;
+import dev.yurisuika.endemic.registry.SaplingRegistry;
 import dev.yurisuika.endemic.server.commands.EndemicCommand;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 import net.neoforged.neoforge.event.RegisterCommandsEvent;
 
@@ -18,6 +20,12 @@ public class Endemic {
         @SubscribeEvent
         public static void registerCommands(RegisterCommandsEvent event) {
             EndemicCommand.register(event.getDispatcher(), event.getBuildContext(), event.getCommandSelection());
+        }
+
+        @SubscribeEvent
+        public static void registerSaplings(FMLCommonSetupEvent event) {
+            SaplingRegistry.bootstrap();
+            SaplingRegistry.loadAllSaplingConfigs();
         }
 
         @SubscribeEvent
