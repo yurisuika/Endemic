6f726b12241c413dd65830f7893be8881717bb14
yurisuika <yurisuika@proton.me>
2025-11-24 16:34:32 -0800

    reworked mixin structure

--- src/main/java/dev/yurisuika/endemic/world/level/block/grower/WeightedTreeGrower.java
+++ src/main/java/dev/yurisuika/endemic/world/level/block/grower/WeightedTreeGrower.java
@@ -1,6 +1,6 @@
 package dev.yurisuika.endemic.world.level.block.grower;
 
-import dev.yurisuika.endemic.mixin.world.level.BiomeInvoker;
+import dev.yurisuika.endemic.mixin.minecraft.world.level.BiomeInvoker;
 import dev.yurisuika.endemic.util.Configure;
 import dev.yurisuika.endemic.util.LightSource;
 import dev.yurisuika.endemic.util.Locate;
